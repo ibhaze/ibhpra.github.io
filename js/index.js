3e7d6533701835f5886ec2636452b519fc747030
ibhaze <130766601+ibhaze@users.noreply.github.com>
2024-03-15 17:42:44 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// index.js
+const oneLinerJoke = require('one-liner-joke');
+// Get a random joke
+const getRandomJoke = oneLinerJoke.getRandomJoke();
+// Log the joke to the 
+console.log(getRandomJoke.body);
